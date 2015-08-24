1391713e46cc37f63f6c5e6c35f02dab7b55c1da
Noah Watkins <noahwatkins@gmail.com>
2015-08-23 20:12:27 -0700

    libzlog: support async append
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/libzlog.cc
+++ src/libzlog.cc
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <sstream>
 #include <string>
+#include <condition_variable>
 #include <rados/librados.hpp>
 #include <cls_zlog_client.h>
 #include "libzlog.h"
@@ -242,6 +243,241 @@ int Log::CheckTail(uint64_t *pposition, bool increment)
     return ret;
   }
   assert(0);
+}
+
+struct zlog::Log::AioCompletionImpl {
+  std::condition_variable cond;
+  std::mutex lock;
+  int ref;
+  bool complete;
+  bool released;
+
+  // AioAppend result
+  int retval;
+
+  // AioAppend callback
+  Log::AioCompletion::callback_t safe_cb;
+  void *safe_cb_arg;
+
+  // Data being appended
+  ceph::bufferlist bl;
+
+  // Current append position
+  uint64_t position;
+
+  // Final position result
+  uint64_t *pposition;
+
+  Log *log;
+  librados::IoCtx *ioctx;
+  librados::AioCompletion *c;
+
+  AioCompletionImpl() :
+    ref(1), complete(false), released(false), retval(0)
+  {}
+
+  int wait_for_complete() {
+    std::unique_lock<std::mutex> l(lock);
+    cond.wait(l, [&]{ return complete; });
+    return 0;
+  }
+
+  int get_return_value() {
+    std::lock_guard<std::mutex> l(lock);
+    return retval;
+  }
+
+  void release() {
+    lock.lock();
+    assert(!released);
+    released = true;
+    put_unlock();
+  }
+
+  void put_unlock() {
+    assert(ref > 0);
+    int n = --ref;
+    lock.unlock();
+    if (!n)
+      delete this;
+  }
+
+  void get() {
+    std::lock_guard<std::mutex> l(lock);
+    assert(ref > 0);
+    ref++;
+  }
+
+  void set_callback(void *arg, zlog::Log::AioCompletion::callback_t cb) {
+    std::lock_guard<std::mutex> l(lock);
+    safe_cb = cb;
+    safe_cb_arg = arg;
+  }
+};
+
+void zlog::Log::AioCompletion::wait_for_complete()
+{
+  AioCompletionImpl *impl = (AioCompletionImpl*)pc;
+  impl->wait_for_complete();
+}
+
+int zlog::Log::AioCompletion::get_return_value()
+{
+  zlog::Log::AioCompletionImpl *impl = (AioCompletionImpl*)pc;
+  return impl->get_return_value();
+}
+
+void zlog::Log::AioCompletion::release()
+{
+  zlog::Log::AioCompletionImpl *impl = (AioCompletionImpl*)pc;
+  impl->release();
+  delete this;
+}
+
+void zlog::Log::AioCompletion::set_callback(void *arg,
+    zlog::Log::AioCompletion::callback_t cb)
+{
+  zlog::Log::AioCompletionImpl *impl = (AioCompletionImpl*)pc;
+  impl->set_callback(arg, cb);
+}
+
+/*
+ *
+ */
+void aio_safe_cb(librados::completion_t cb, void *arg)
+{
+  zlog::Log::AioCompletionImpl *impl = (zlog::Log::AioCompletionImpl*)arg;
+  librados::AioCompletion *rc = impl->c;
+  bool finish = false;
+
+  impl->lock.lock();
+
+  int ret = rc->get_return_value();
+
+  // done with the rados completion
+  rc->release();
+
+  if (ret == zlog::CLS_ZLOG_OK) {
+    /*
+     * Append was successful. We're done.
+     */
+    if (impl->pposition)
+      *impl->pposition = impl->position;
+    ret = 0;
+    finish = true;
+  } else if (ret == zlog::CLS_ZLOG_STALE_EPOCH) {
+    /*
+     * We'll need to try again with a new epoch.
+     */
+    ret = impl->log->RefreshProjection();
+    if (ret)
+      finish = true;
+  } else if (ret < 0) {
+    /*
+     * Encountered a RADOS error.
+     */
+    finish = true;
+  } else {
+    assert(ret == zlog::CLS_ZLOG_READ_ONLY);
+  }
+
+  /*
+   * Try append again with a new position. This can happen if above there is a
+   * stale epoch that we refresh, or if the position was marked read-only.
+   */
+  if (!finish) {
+    uint64_t position;
+    ret = impl->log->CheckTail(&position, true);
+    if (ret)
+      finish = true;
+    else {
+      impl->position = position;
+
+      // build new rados completion
+      impl->c = librados::Rados::aio_create_completion(impl, NULL, aio_safe_cb);
+      assert(impl->c);
+      // don't need impl->get(): reuse reference
+
+      // build new write op
+      librados::ObjectWriteOperation op;
+      zlog::cls_zlog_write(op, impl->log->epoch_, impl->position, impl->bl);
+
+      // submit
+      std::string oid = impl->log->position_to_oid(position);
+      ret = impl->ioctx->aio_operate(oid, impl->c, &op);
+      if (ret)
+        finish = true;
+    }
+  }
+
+  // complete aio if append success, or any error
+  if (finish) {
+    impl->retval = ret;
+    impl->complete = true;
+    impl->lock.unlock();
+    if (impl->safe_cb)
+      impl->safe_cb(impl, impl->safe_cb_arg);
+    impl->cond.notify_all();
+    impl->lock.lock();
+    impl->put_unlock();
+    return;
+  }
+
+  impl->lock.unlock();
+}
+
+Log::AioCompletion *Log::aio_create_completion(void *arg,
+    Log::AioCompletion::callback_t cb)
+{
+  AioCompletionImpl *impl = new AioCompletionImpl;
+  impl->safe_cb = cb;
+  impl->safe_cb_arg = arg;
+  return new AioCompletion(impl);
+}
+
+Log::AioCompletion *Log::aio_create_completion()
+{
+  return aio_create_completion(NULL, NULL);
+}
+
+/*
+ * The retry for AioAppend is coordinated through the aio_safe_cb callback
+ * which will dispatch a new rados operation.
+ */
+int Log::AioAppend(AioCompletion *c, ceph::bufferlist& data,
+    uint64_t *pposition)
+{
+  // initial position guess
+  uint64_t position;
+  int ret = CheckTail(&position, true);
+  if (ret)
+    return ret;
+
+  AioCompletionImpl *impl = (AioCompletionImpl*)c->pc;
+
+  impl->log = this;
+  impl->bl = data;
+  impl->position = position;
+  impl->pposition = pposition;
+  impl->ioctx = ioctx_;
+
+  impl->get(); // rados aio now has a reference
+  impl->c = librados::Rados::aio_create_completion(impl, NULL, aio_safe_cb);
+  assert(impl->c);
+
+  librados::ObjectWriteOperation op;
+  zlog::cls_zlog_write(op, epoch_, position, data);
+
+  std::string oid = position_to_oid(position);
+  ret = ioctx_->aio_operate(oid, impl->c, &op);
+  /*
+   * Currently aio_operate never fails. If in the future that changes then we
+   * need to make sure that references to impl and the rados completion are
+   * cleaned up correctly.
+   */
+  assert(ret == 0);
+
+  return ret;
 }
 
 int Log::Append(ceph::bufferlist& data, uint64_t *pposition)
