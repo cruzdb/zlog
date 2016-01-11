7ea8d86cd8d699f96e730e2a33d16d5cd1c15dc5
Noah Watkins <noahwatkins@gmail.com>
2016-01-11 15:34:15 -0800

    ci: update docker
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -2,19 +2,33 @@
 set -e
 set -x
 
+# checkout zlog ceph repo
+pushd /src/ceph
+git remote add nd https://github.com/noahdesu/ceph.git
+git fetch nd
+git checkout -b zlog/jewel nd/zlog/jewel
+git submodule update --force --init --recursive
+bash ./install-deps.sh
+
+# build and install zlog object class and client
+./autogen.sh
+./configure
+cd src
+make libcls_zlog.la
+make libcls_zlog_client.la
+cp -a .libs/libcls_zlog.so* /usr/lib/rados-classes/
+cp -a .libs/libcls_zlog_client.so* /usr/lib/
+cp cls/zlog/cls_zlog_client.h /usr/include/rados/
+popd
+
 # startup a ceph cluster
 mkdir /tmp/ceph
 bash /src/micro-osd.sh /tmp/ceph
 
 BRANCH=${branch:-master}
 
-apt-get install -y libprotobuf-dev protobuf-compiler
-
-# install zlog. this is done in the entry point so it will
-# pull the latest zlog source each time the container is run
+# build zlog
 cd /src
-
-ls -l
 
 if [ ! -d /src/zlog ]; then
   git clone --branch=$BRANCH https://github.com/noahdesu/zlog.git
@@ -27,6 +41,7 @@ autoreconf -ivf
 ./configure
 make
 
+# run seqr and tests
 cd /src/zlog/src
 export CEPH_CONF=/tmp/ceph/ceph.conf
 ./zlog-seqr --port 5678 --daemon
