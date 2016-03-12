96748c98ff678e6fa26dd7f6ecdee3de34e14def
Noah Watkins <noahwatkins@gmail.com>
2016-03-11 16:34:20 -0800

    bench: add direct payload mapping benchmark
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ src/bench/common.h
@@ -0,0 +1,18 @@
+#ifndef ZLOG_SRC_BENCH_COMMON_H
+#define ZLOG_SRC_BENCH_COMMON_H
+
+static inline uint64_t __getns(clockid_t clock)
+{
+  struct timespec ts;
+  int ret = clock_gettime(clock, &ts);
+  assert(ret == 0);
+  return (((uint64_t)ts.tv_sec) * 1000000000ULL) + ts.tv_nsec;
+}
+
+static inline uint64_t getns()
+{
+  return __getns(CLOCK_MONOTONIC);
+}
+
+
+#endif
