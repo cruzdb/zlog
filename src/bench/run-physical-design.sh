a9d62b0e623525aa6d3f418e6bda4aef3f3a2882
Noah Watkins <noahwatkins@gmail.com>
2016-04-03 16:36:23 -0700

    bench: add interface variant runner
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/bench/run-physical-design.sh
+++ src/bench/run-physical-design.sh
@@ -9,12 +9,15 @@ stripe_width=$3
 entry_size=$4
 qdepth=$5
 waitsec=$6
+interface=$7
+#prefix=$8
 
 perf_file="pool-${pool}_expr-${experiment}"
 perf_file="${perf_file}_sw-${stripe_width}"
 perf_file="${perf_file}_es-${entry_size}"
 perf_file="${perf_file}_qd-${qdepth}"
 perf_file="${perf_file}_rt-${waitsec}"
+perf_file="${perf_file}_if-${interface}"
 perf_file="${perf_file}.log"
 
 args="--pool $pool"
@@ -25,7 +28,8 @@ args="$args --qdepth $qdepth"
 args="$args --tp 2"
 args="$args --perf_file $perf_file"
 args="$args --runtime $waitsec"
-args="$args --prefix pre1"
+args="$args --interface $interface"
+#args="$args --prefix $prefix"
 
 echo "Running: ./physical-design $args"
 ./physical-design $args
