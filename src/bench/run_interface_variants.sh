8b823f8930b07a0961dde2f2e85c5351c54ade94
Noah Watkins <noahwatkins@gmail.com>
2016-04-04 08:16:11 -0600

    bench: setup iface variant runner
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/bench/run_interface_variants.sh
+++ src/bench/run_interface_variants.sh
@@ -2,15 +2,21 @@
 set -e
 set -x
 
+ddev=/dev/sdc
+jdev=none
+pgnum=128
+version=jewel
 pool=zlog
-waitsec=30
-qdepth=12
-stripe_width=10
-entry_size=100
+waitsec=3600
+qdepth=16
+stripe_width=128
+entry_size=1024
 
 exp="map_n1"
 interfaces="vanilla cls_no_index cls_full"
 for interface in $interfaces; do
+  bench/soft-reset-ceph.sh $ddev $jdev $pool $pgnum $version
+  sleep 300
   bench/run-physical-design.sh $pool $exp $stripe_width $entry_size \
     $qdepth $waitsec $interface
 done
@@ -18,6 +24,8 @@ done
 exp="bytestream_n1_write"
 interfaces="vanilla cls_no_index cls_full"
 for interface in $interfaces; do
+  bench/soft-reset-ceph.sh $ddev $jdev $pool $pgnum $version
+  sleep 300
   bench/run-physical-design.sh $pool $exp $stripe_width $entry_size \
     $qdepth $waitsec $interface
 done
@@ -25,6 +33,8 @@ done
 exp="bytestream_n1_append"
 interfaces="vanilla cls_no_index cls_check_epoch cls_full"
 for interface in $interfaces; do
+  bench/soft-reset-ceph.sh $ddev $jdev $pool $pgnum $version
+  sleep 300
   bench/run-physical-design.sh $pool $exp $stripe_width $entry_size \
     $qdepth $waitsec $interface
 done
