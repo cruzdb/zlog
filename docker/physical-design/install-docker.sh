31bcbe0d4528d506b40fb374985d63c7ca49a431
Noah Watkins <noahwatkins@gmail.com>
2016-04-16 16:34:38 -0700

    Update install-docker.sh

--- docker/physical-design/install-docker.sh
+++ docker/physical-design/install-docker.sh
@@ -8,8 +8,8 @@ if ! which docker &> /dev/null; then
   sudo apt-get -y install apt-transport-https ca-certificates
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" \
-    > /etc/apt/sources.list.d/docker.list
+  echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee \
+    /etc/apt/sources.list.d/docker.list
   sudo apt-get -y update
   sudo apt-get -y install docker-engine
   sudo service docker start  
