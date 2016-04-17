e39fee1c7ce59675cb30726eab9e1fda3b226208
Noah Watkins <noahwatkins@gmail.com>
2016-04-17 17:09:02 -0500

    bench: add ceph rest option
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- docker/physical-design/run-throughput-sweep.sh
+++ docker/physical-design/run-throughput-sweep.sh
@@ -3,12 +3,13 @@
 set -e
 
 name=tpsweep
-runtime=30
+runtime=300
 pg_nums="32 128"
-stripe_widths="1 32 128"
-queue_depths="1 4 8 16 32"
+stripe_widths="32 128"
+queue_depths="1 8 16 32"
 entry_sizes="512"
 pool=zlog
+rest=120
 
 # workloads and i/o interfaces
 workloads="bytestream_n1_append"
