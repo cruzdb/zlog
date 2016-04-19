b7a5619fbe0e873fdc9ad0e413b6cdb4d98a8161
Noah Watkins <noahwatkins@gmail.com>
2016-04-18 23:37:22 -0500

    bench: remove run-all.sh
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- docker/physical-design/runner.sh
+++ docker/physical-design/runner.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-
 set -e
+
+#
+# Example script
+#
+# name=all
+# runtime=10
+# pg_nums="128"
+# stripe_widths="5"
+# queue_depths="4"
+# entry_sizes="4096"
+# pool=zlog
+#
+# # workloads
+# wl_11="map_11 bytestream_11"
+# wl_n1="map_n1 bytestream_n1_write bytestream_n1_append"
+# workloads="$wl_11 $wl_n1"
+#
+# # i/o interfaces
+# map_n1_if="vanilla cls_no_index cls_no_index_wronly cls_full"
+# bytestream_n1_write_if="vanilla cls_no_index cls_no_index_wronly cls_full cls_full_hdr_idx cls_full_inline_idx"
+# bytestream_n1_append_if="vanilla cls_no_index cls_no_index_wronly cls_check_epoch cls_check_epoch_hdr cls_full cls_full_hdr_idx cls_no_index_wronly_xtn"
+#
+# . runner.sh
+# run_pd
+#
 
 # name of results dir
 logdir=$PWD/results.pd.${name}.$(hostname --short).$(date +"%m-%d-%Y_%H-%M-%S")
