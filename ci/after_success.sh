04dd4656e8177385a91251698065bb15c8f1bee0
Noah Watkins <noahwatkins@gmail.com>
2016-08-18 10:17:22 -0700

    ci: build and run outside docker
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-#coveralls --exclude lib --exclude tests --gcov-options '\-lp'
+if [ "${RUN_COVERAGE}" == 1 ]; then
+  pushd build
+  coveralls --exclude src/googletest --exclude src/proto \
+    --exclude src/kvstore/kvstore.pb.h --exclude src/kvstore/kvstore.pb.cc \
+    --gcov-options '\-lp'
+  popd
+fi
--- ci/script.sh
+++ ci/script.sh
@@ -2,19 +2,75 @@
 
 set -e
 
-# we don't run any tests for a coverity build
-if [ "${COVERITY_SCAN_BRANCH}" == 1 ]; then
-  exit 0
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+  #
+  # Build and cache the ZLog plugin for Ceph?
+  #
+  if [ ! -f "$HOME/zlog_ceph_deps/cls_zlog_client.h" ]; then
+    #
+    # The plugin is built in a docker image
+    #
+cat <<EOF | docker build -t zlog_deps -
+FROM ubuntu:trusty
+
+RUN apt-get update -qq && apt-get install -y git build-essential
+
+RUN mkdir -p /src/ceph && cd /src/ceph && \
+    git clone --recursive --branch=zlog/jewel \
+      https://github.com/noahdesu/ceph.git && \
+    cd ceph && ./install-deps.sh
+
+RUN cd /src/ceph/ceph && ./autogen.sh && \
+    ./configure --prefix=/usr && \
+    cd src && make libcls_zlog.la libcls_zlog_client.la
+EOF
+    # copy artificats back into host
+    mkdir -p $HOME/zlog_ceph_deps
+    docker run -v $HOME/zlog_ceph_deps/:/deps -it zlog_deps \
+      bash -c "cp -a /src/ceph/ceph/src/.libs/libcls_zlog.so* /deps"
+    docker run -v $HOME/zlog_ceph_deps/:/deps -it zlog_deps \
+      bash -c "cp -a /src/ceph/ceph/src/.libs/libcls_zlog_client.so* /deps"
+    docker run -v $HOME/zlog_ceph_deps/:/deps -it zlog_deps \
+      cp /src/ceph/ceph/src/cls/zlog/cls_zlog_client.h /deps
+  fi
+
+  # install (either from cache, or just built in docker above)
+  sudo ls -l $HOME/zlog_ceph_deps/
+  sudo cp -a $HOME/zlog_ceph_deps/libcls_zlog.so* /usr/lib/rados-classes/
+  sudo cp -a $HOME/zlog_ceph_deps/libcls_zlog_client.so* /usr/lib/
+  sudo cp $HOME/zlog_ceph_deps/cls_zlog_client.h /usr/include/rados/
+
+  # start ceph
+  ci/micro-osd.sh /tmp/osd
+  CEPH_CONF=/tmp/osd/ceph.conf ceph status
 fi
 
-if [ "${COVERAGE}" == 1 ]; then
-  cmake -DCMAKE_BUILD_TYPE=Coverage .
-  make
+mkdir build
+pushd build
+
+if [ "${RUN_COVERAGE}" == 1 ]; then
+  cmake -DCMAKE_BUILD_TYPE=Coverage ..
+else
+  cmake -DCMAKE_BUILD_TYPE=Debug ..
+fi
+
+make -j2
+./src/test/zlog-test-ram
+./src/test/db-test
+
+# on linux we assume a ceph instance is running and execute any tests that
+# depend on the ceph backend being available.
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+  CEPH_CONF=/tmp/osd/ceph.conf ./src/zlog-seqr --streams --port 5678 --daemon
+  CEPH_CONF=/tmp/osd/ceph.conf ./src/test/zlog-test-ceph
+fi
+
+if [ "${RUN_COVERAGE}" == 1 ]; then
   make db-test-cov
   make zlog-test-ram-cov
-  coveralls --exclude src/googletest --exclude src/proto \
-    --exclude src/kvstore/kvstore.pb.h --exclude src/kvstore/kvstore.pb.cc \
-    --gcov-options '\-lp'
-else
-  docker run -v $TRAVIS_BUILD_DIR:/src/zlog zlog/ci
+  if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    CEPH_CONF=/tmp/osd/ceph.conf make zlog-test-ceph-cov
+  fi
 fi
+
+popd
