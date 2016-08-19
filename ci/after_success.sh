61b96b494a5c97e9920bb4b1c03f89c873d5cd25
Noah Watkins <noahwatkins@gmail.com>
2016-08-18 17:26:53 -0700

    ci: coverage should use in-tree build
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -3,9 +3,7 @@
 set -e
 
 if [ "${RUN_COVERAGE}" == 1 ]; then
-  pushd build
   coveralls --exclude src/googletest --exclude src/proto \
     --exclude src/kvstore/kvstore.pb.h --exclude src/kvstore/kvstore.pb.cc \
     --gcov-options '\-lp'
-  popd
 fi
