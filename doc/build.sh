beddfdbc4a2b8b230d5b7bcbf978f7913043c199
Noah Watkins <noahwatkins@gmail.com>
2017-02-13 11:03:28 -0800

    doc: check deps on debian
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- doc/build.sh
+++ doc/build.sh
@@ -5,16 +5,41 @@ cd "$(dirname "$0")"
 cd ..
 TOPDIR=`pwd`
 
-for package in python-devel python-pip python2-virtualenv; do
-  if ! rpm -q $package >/dev/null ; then
-    missing="${missing:+$missing }$package"
+if command -v dpkg > /dev/null; then
+  for package in python-dev python-pip python-virtualenv; do
+    if [ "$(dpkg --status -- $package 2>&1 | sed -n 's/^Status: //p')" != "install ok installed" ]; then
+        missing="${missing:+$missing }$package"
+    fi
+  done
+  if [ -n "$missing" ]; then
+    echo "$0: missing required packages, please install them:" 1>&2
+    echo "sudo apt-get install -o APT::Install-Recommends=true $missing" 1>&2
+    exit 1
   fi
-done
-
-if [ -n "$missing" ]; then
-  echo "$0: missing required packages, please install them:" 1>&2
-  echo "yum/dnf install -y $missing"
-  exit 1
+elif command -v yum > /dev/null; then
+  for package in python-devel python-pip python2-virtualenv; do
+    if ! rpm -q $package >/dev/null ; then
+      missing="${missing:+$missing }$package"
+    fi
+  done
+  if [ -n "$missing" ]; then
+    echo "$0: missing required packages, please install them:" 1>&2
+    echo "yum install -y $missing"
+    exit 1
+  fi
+else
+  for command in virtualenv; do
+    command -v "$command" > /dev/null;
+    ret_code=$?
+    if [ $ret_code -ne 0 ]; then
+      missing="${missing:+$missing }$command"
+    fi
+  done
+  if [ -n "$missing" ]; then
+    echo "$0: missing required command, please install them:" 1>&2
+    echo "$missing"
+    exit 1
+  fi
 fi
 
 set -e
