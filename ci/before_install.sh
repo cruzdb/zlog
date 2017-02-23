434d8287adb32c757329497fdc679510681fecb5
Noah Watkins <noahwatkins@gmail.com>
2017-02-22 18:29:51 -0800

    cmake: use newer version of cmake with create_javah
    We could work around this if we really needed an older version of cmake.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -6,12 +6,13 @@ set -x
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZLOG_DIR=${THIS_DIR}/../
 
-# build ceph plugin
-pushd ${ZLOG_DIR}/docker/ceph-plugin
-docker rm built-ceph-plugin || true
-docker build -t ceph-plugin .
-docker run -v /ceph_zlog_plugin --name built-ceph-plugin ceph-plugin
-popd
+# solution adapted from:
+#  http://stackoverflow.com/questions/33196136/travis-ci-update-cmake-using-the-packages-cache
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+  CMAKE_URL="https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.tar.gz"
+  wget --quiet -O - ${CMAKE_URL} | sudo tar --strip-components=1 -xz -C /usr
+fi
+# osx has up-to-date cmake
 
 # start micro-osd with plugin
 pushd ${ZLOG_DIR}/docker/micro-osd
