7ec29d4dc4d1e8fed473e08eb991760c6beab440
Noah Watkins <noahwatkins@gmail.com>
2017-02-22 18:30:52 -0800

    ci: only run ceph tests on linux
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -14,9 +14,19 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
 fi
 # osx has up-to-date cmake
 
-# start micro-osd with plugin
-pushd ${ZLOG_DIR}/docker/micro-osd
-docker build -t micro-osd .
-docker run --rm -d --net=host --volumes-from built-ceph-plugin \
-  -v /tmp/micro-osd:/micro-osd micro-osd
-popd
+# skip ceph tests on osx
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+  # build ceph plugin
+  pushd ${ZLOG_DIR}/docker/ceph-plugin
+  docker rm built-ceph-plugin || true
+  docker build -t ceph-plugin .
+  docker run -v /ceph_zlog_plugin --name built-ceph-plugin ceph-plugin
+  popd
+
+  # start micro-osd with plugin
+  pushd ${ZLOG_DIR}/docker/micro-osd
+  docker build -t micro-osd .
+  docker run --rm -d --net=host --volumes-from built-ceph-plugin \
+    -v /tmp/micro-osd:/micro-osd micro-osd
+  popd
+fi
