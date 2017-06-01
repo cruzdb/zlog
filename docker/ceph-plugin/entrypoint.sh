c2572d5375980ab5727aa0605d2a80b77444d256
Noah Watkins <noahwatkins@gmail.com>
2017-06-01 09:36:02 -0700

    docker: build out-of-tree ceph plugin
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- docker/ceph-plugin/entrypoint.sh
+++ docker/ceph-plugin/entrypoint.sh
@@ -3,18 +3,6 @@
 set -e
 set -x
 
-tar xzf ceph.tar.gz
-
-pushd /ceph
-git fetch origin
-git checkout -b build origin/zlog/master-pb
-
-apt-get update
-./install-deps.sh
-./do_cmake.sh
-
-pushd build
-make -j$(nproc) cls_zlog
-
-rm -rf /ceph_zlog_plugin/*
-cp -a lib/libcls_zlog.so* /ceph_zlog_plugin/
+pushd /src/zlog
+cmake .
+make cls_zlog
