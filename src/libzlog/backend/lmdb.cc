8dd6787a596fc0823f935affc36242b3e8a64b45
Noah Watkins <noahwatkins@gmail.com>
2017-06-12 16:23:14 -0700

    lmdb: configurable db size
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/libzlog/backend/lmdb.cc
+++ src/libzlog/backend/lmdb.cc
@@ -465,7 +465,14 @@ void LMDBBackend::Init(const std::string& path, bool empty)
   ret = mdb_env_set_maxdbs(env, 2);
   assert(ret == 0);
 
-  ret = mdb_env_set_mapsize(env, 1<<30);
+  size_t gbs = 1;
+  char *size_str = getenv("ZLOG_LMDB_BE_SIZE");
+  if (size_str) {
+    gbs = atoi(size_str);
+  }
+
+  gbs = gbs << 30;
+  ret = mdb_env_set_mapsize(env, gbs);
   assert(ret == 0);
 
   unsigned int flags = MDB_MAPASYNC | MDB_WRITEMAP | MDB_NOMEMINIT;
