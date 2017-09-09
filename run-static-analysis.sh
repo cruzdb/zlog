059097c4225455393feab460f21ad8ef7a2b07c7
Noah Watkins <noahwatkins@gmail.com>
2017-09-09 09:47:16 -0700

    tool: add static analysis script
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ run-static-analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# see github.com/noahdesu/docks/cppcheck
+docker run --rm -i -v $PWD:/usr/src/zlog:ro cppcheck \
+  cppcheck --force --inconclusive --enable=all /usr/src/zlog \
+  -i/usr/src/zlog/src/spdlog -i/usr/src/zlog/src/rapidjson \
+  -i/usr/src/zlog/src/googletest
+
+#docker run --rm -v $PWD:/usr/src/zlog:ro iwyu \
+#  /bin/bash -c "/usr/src/zlog/install-deps.sh && mkdir /build && cd /build && \
+#    echo $CC $CXX && cmake -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=/usr/src/iwyu /usr/src/zlog && \
+#    make"
