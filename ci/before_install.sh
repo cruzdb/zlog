2f3441a167d762d63e3aa8390f8d19a5ddb454cf
Noah Watkins <noahwatkins@gmail.com>
2017-09-13 18:47:54 -0700

    ci: build and run micro-osd with zlog plugin
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -6,28 +6,30 @@ set -x
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZLOG_DIR=${THIS_DIR}/../
 
-## spin-up a ceph osd
-#if [[ "${TRAVIS_BRANCH}" != "coverity_scan" ]]; then
-#if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
-#
-#  # build the zlog ceph-plugin builder image
-#  pushd ${ZLOG_DIR}/docker/ceph-plugin
-#  docker build -t ceph-plugin .
-#  popd
-#
-#  # build micro-osd docker image
-#  pushd ${ZLOG_DIR}/docker/micro-osd
-#  docker build -t micro-osd .
-#  popd
-#
-#  # build the zlog ceph plugin from source
-#  docker run -v /ceph_plugin -v ${ZLOG_DIR}:/src/zlog \
-#      --name ceph-plugin-built ceph-plugin
-#
-#  # run the micro-osd with the built plugin
-#  docker run -d --net=host --volumes-from ceph-plugin-built \
-#    --name micro-osd -v /tmp/micro-osd:/micro-osd micro-osd
-#
-#  sleep 10
-#fi
-#fi
+# spin-up a ceph osd
+if [[ "${TRAVIS_BRANCH}" != "coverity_scan" ]]; then
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+
+  # build the zlog ceph-plugin builder image
+  pushd ${ZLOG_DIR}/docker/ceph-plugin
+  docker build -t ceph-plugin .
+  popd
+
+  # build micro-osd docker image
+  pushd ${ZLOG_DIR}/docker/micro-osd
+  docker build -t micro-osd .
+  popd
+
+  # build the zlog ceph plugin from source
+  docker run -v /ceph_plugin -v ${ZLOG_DIR}:/src/zlog \
+      --name ceph-plugin-built ceph-plugin
+
+  # run the micro-osd with the built plugin
+  docker run -d --net=host --volumes-from ceph-plugin-built \
+    --name micro-osd -v /tmp/micro-osd:/micro-osd micro-osd
+
+  sleep 10
+
+  ls -l /tmp/micro-osd
+fi
+fi
