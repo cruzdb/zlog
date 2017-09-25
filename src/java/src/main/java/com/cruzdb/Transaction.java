2ca8aab03c43a5c171e5d232dca70cbb13d7faf3
Noah Watkins <noahwatkins@gmail.com>
2017-09-25 09:58:50 -0700

    java: fixup javadoc warnings
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/java/src/main/java/com/cruzdb/Transaction.java
+++ src/java/src/main/java/com/cruzdb/Transaction.java
@@ -17,35 +17,40 @@ public class Transaction extends ZObject {
   }
 
   /**
-   *
+   * @param key the key of the entry.
+   * @param value the value associated with the key.
+   * @throws com.cruzdb.LogException if an error occurs in the native library.
    */
   public void put(final byte[] key, final byte[] value) throws LogException {
     put(nativeHandle_, key, 0, key.length, value, 0, value.length);
   }
 
   /**
-   *
+   * @param key the key of the entry.
+   * @return the value associated with the key.
+   * @throws com.cruzdb.LogException if an error occurs in the native library.
    */
   public byte[] get(final byte[] key) throws LogException {
     return get(nativeHandle_, key, 0, key.length);
   }
 
   /**
-   *
+   * @param key the key to delete.
+   * @throws com.cruzdb.LogException if an error occurs in the native library.
    */
   public void delete(final byte[] key) throws LogException {
     delete(nativeHandle_, key, 0, key.length);
   }
 
   /**
-   *
+   * @throws com.cruzdb.LogException if an error occurs in the native library.
    */
   public void commit() throws LogException {
     commit(nativeHandle_);
   }
 
   /**
-   *
+   * @throws com.cruzdb.LogException if an error occurs in the native library.
    */
   public void abort() throws LogException {
     abort(nativeHandle_);
