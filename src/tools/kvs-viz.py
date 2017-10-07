9ca5a3ae634321204c462ba8656581154399d572
Noah Watkins <noahwatkins@gmail.com>
2017-10-07 12:37:52 -0700

    kvs: visualize based on address
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/tools/kvs-viz.py
+++ src/tools/kvs-viz.py
@@ -1,3 +1,6 @@
+# todo: color code partitions
+# todo: size/color as function of hops from root
+# todo: d3 in browser viz
 import sys
 from graphviz import Digraph
 import kvs
@@ -5,16 +8,19 @@ import kvs
 log = kvs.Log(sys.stdin)
 snapshots = log.snapshots()
 
+colors = ['red', 'black', 'blue', 'green', 'purple']
+
 for snapshot in snapshots[0:1000]:
     u = Digraph(`snapshot`.zfill(8), format="png",
             graph_attr={'ratio': 'fill',
                         'size': '15,10'},
             node_attr={'fontcolor': 'white'})
-    db = kvs.Database(log, 1, snapshot)
+    db = kvs.Database(log, len(colors), snapshot)
     for node in db.preorder():
         u.node(node.address(), label=`int(node.key)`,
                 style='filled',
-                color='red' if node.red else 'black')
+                color=colors[db.dist.address(node.pos)])
+                #color='red' if node.red else 'black')
         if not node.left.nil: u.edge(node.address(), node.left.address())
         if not node.right.nil: u.edge(node.address(), node.right.address())
     u.render()
