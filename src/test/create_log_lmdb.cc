99068d826b63ae454b3553f97b0774f10e75bbb0
Noah Watkins <noahwatkins@gmail.com>
2018-03-06 16:44:00 -0800

    log: wait for views to finish updating
    the view update thread was notifying waiters before it was fully up to
    date. this resulted in a racing thread seeing an old epoch value. this
    patch makes waiters wait until at least one instance of requesting new
    views and not receving them.
    
    fixes #208
    
    thanks to @javierron for reporting!
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ src/test/create_log_lmdb.cc
@@ -0,0 +1,19 @@
+#include <zlog/log.h>
+
+int main(int argc, char **argv)
+{
+  zlog::Log *log;
+  int ret = zlog::Log::Create("lmdb", "mylog",
+      {{"path", "/tmp/zlog.tmp.db"}}, "", "", &log);
+  assert(ret == 0);
+
+  const std::string input = "oh the input";
+
+  uint64_t pos;
+  ret = log->Append(zlog::Slice(input), &pos);
+  assert(ret == 0);
+
+  delete log;
+
+  return 0;
+}
