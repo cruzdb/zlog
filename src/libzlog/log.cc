9429bd70d839185adbe6f5916475e226356ef2e6
Noah Watkins <noahwatkins@gmail.com>
2018-10-04 19:51:26 -0700

    cleanup: move relevant todo items to project
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/libzlog/log.cc
+++ src/libzlog/log.cc
@@ -16,8 +16,6 @@ Log::~Log() {}
 int Log::Open(const Options& options,
     const std::string& name, Log **logpp)
 {
-  // TODO: validate options
-
   if (name.empty()) {
     return -EINVAL;
   }
@@ -49,7 +47,6 @@ int Log::Open(const Options& options,
         return -EEXIST;
       }
 
-      // TODO: make_unique
       impl = std::unique_ptr<LogImpl>(
           new LogImpl(backend, name, hoid, prefix, options));
 
@@ -59,15 +56,8 @@ int Log::Open(const Options& options,
         return -ENOENT;
       }
 
-      // TODO: create an option that controls how much state is stuffed into the
-      // initial view, versus configured after creation, either in this method
-      // or later by users of the log. this is an optimization that should be
-      // added later after lots of testing without it. Currently the initial
-      // view has no settings at all (no stripes, or sequencer info).
       const auto init_view = View::create_initial();
 
-      // TODO: document that CreateLog is exclusive
-      // TODO: return hoid if creation is successful
       ret = backend->CreateLog(name, init_view, hoid, prefix);
       if (ret) {
         if (ret == -EEXIST) {
@@ -80,9 +70,6 @@ int Log::Open(const Options& options,
           return ret;
         }
       } else {
-        // TODO: hoid here comes from CreateLog
-        // TODO: actually... look at OpenLog. we may want to do OpenLog and then
-        // verify that the log we opened has the same hoid.
         created = true;
         impl = std::unique_ptr<LogImpl>(
             new LogImpl(backend, name, hoid, prefix, options));
@@ -90,58 +77,11 @@ int Log::Open(const Options& options,
     }
   }
 
-  // ensure the log has storage mappings up to the prefault position. when a
-  // prefault position is not defined and the log has no mappings (e.g. it was
-  // just newly created) then the first mapping will be created in response to a
-  // write operation. this is only useful for setting up test scenarios.
   if (options.prefault_position) {
     // call something like ensure mapping
   }
 
   (void)created;
-
-#if 0
-  // build the initial view
-  std::string seq_cookie;
-  std::string seq_host;
-  std::string seq_port;
-
-  if (host.empty()) {
-    auto uuid = boost::uuids::random_generator()();
-    auto hostname = boost::asio::ip::host_name();
-    std::stringstream exclusive_cookie_ss;
-    exclusive_cookie_ss << uuid << "." << hostname
-      << "." << 0;
-    seq_cookie = exclusive_cookie_ss.str();
-  } else {
-    seq_host = host;
-    seq_port = port;
-  }
-
-  // TODO: striper::create_initial(...);
-  //auto init_view = Striper::InitViewData(options.width, options.entries_per_object);
-  auto init_view = View::create_initial(
-      options.width, options.entries_per_object,
-      seq_cookie, seq_host, seq_port);
-
-  // make sure to set before update view. TODO: this is probably a bug since we
-  // don't really know that the initial view is the one that is active. almost
-  // certainly a bug.
-  if (host.empty()) {
-    impl->exclusive_cookie = seq_cookie;
-    impl->exclusive_empty = true;
-    impl->exclusive_position = 0;
-  }
-#endif
-
-  // if we created the log then we can set the sequencer here based on options
-  // that we have available. we can also NOT set the sequencer info, and it can
-  // be created later in response to the client invoking log operations. like
-  // prefault position being unspecified above, this is just for testing.
-
-  // if the log already existed, then we want to give priority to whatever
-  // sequencer is specified in the log, if any. if none is specified (no view or
-  // its just empty), then fall back to options or defaults.
 
   *logpp = impl.release();
 
