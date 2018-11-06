2331d38612f4d7882ca763babaca759e385a3192
Noah Watkins <noahwatkins@gmail.com>
2018-11-05 19:10:22 -0800

    ci: add basic jenkins setup
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- ci/script.sh
+++ ci/script.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
 set -e
-set -x
 
-COVERAGE_ENV=""
-if [ "${RUN_COVERAGE}" == 1 ]; then
-  COVERAGE_ENV=`bash <(curl -s https://codecov.io/env)`
-fi
+this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+proj_dir=${this_dir}/../
 
-if [ ! -z ${DOCKER_IMAGE+x} ]; then
-  docker run --net=host --rm -v ${TRAVIS_BUILD_DIR}:/zlog:z,ro \
-    -w /zlog ${COVERAGE_ENV} -e RUN_COVERAGE \
-    -v /tmp/micro-osd:/tmp/micro-osd:z ${DOCKER_IMAGE} \
-    /bin/bash -c "env && ./install-deps.sh && ./ci/install-ceph.sh && ./ci/run.sh"
-else
-  ${TRAVIS_BUILD_DIR}/install-deps.sh
-  ${TRAVIS_BUILD_DIR}/ci/install-ceph.sh
-  ${TRAVIS_BUILD_DIR}/ci/run.sh
-fi
+. ${this_dir}/common.sh
+
+base_img=${1}
+img_tag=$(build_deps_image_tag ${base_img})
+
+docker run --rm -v ${proj_dir}:/src/zlog:z,ro \
+  -w /src/zlog ${img_tag} /bin/bash -c "ci/test.sh"
