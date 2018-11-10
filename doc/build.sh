863e7a5e2e85f5f8b85ffa103efaee4c9f78aa5d
Noah Watkins <noahwatkins@gmail.com>
2018-11-10 15:35:14 -0800

    doc: try other venv forms
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- doc/build.sh
+++ doc/build.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 # adapted from https://github.com/ceph/ceph/blob/master/admin/build-doc
-
 set -e
+set -x
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZLOG_DIR=${THIS_DIR}/../
 OUTPUT_DIR=${1:-"${ZLOG_DIR}/build-doc"}
 
-for command in virtualenv; do
-  command -v "$command" > /dev/null;
-  ret_code=$?
-  if [ $ret_code -ne 0 ]; then
-    missing="${missing:+$missing }$command"
-  fi
-done
-if [ -n "$missing" ]; then
-  echo "$0: missing required command, please install them:" 1>&2
-  echo "$missing"
-  exit 1
-fi
-
 mkdir -p ${OUTPUT_DIR}
 if [ ! -e ${OUTPUT_DIR}/virtualenv ]; then
-  virtualenv --no-site-packages ${OUTPUT_DIR}/virtualenv
+  set +e
+  virtualenv ${OUTPUT_DIR}/virtualenv
+  if [ $? -ne 0 ]; then
+    set -e
+    python3 -m venv ${OUTPUT_DIR}/virtualenv
+  fi
+  set -e
   ${OUTPUT_DIR}/virtualenv/bin/pip install -U pip
 fi
 
